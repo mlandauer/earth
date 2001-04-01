b448ca772b4d094ba5b3fc41fa59dd828de80a0b
mlandauer <mlandauer>
2001-04-01 22:08:42 +0000

    r44: Almost returns correct thing for directory listing

--- SpDir.C
+++ SpDir.C
@@ -1,8 +1,15 @@
 // $Id$
 
 #include "SpDir.h"
+#include "SpFile.h"
 
-SpDir::SpDir()
+#include <list>
+#include <sys/types.h>
+#include <dirent.h>
+
+#include "SpFsObject.h"
+
+SpDir::SpDir(const string &path) : SpFsObject(path)
 {
 }
 
@@ -10,9 +17,6 @@ SpDir::~SpDir()
 {
 }
 
-SpDir::SpDir(const string &path) : SpFsObject(path)
-{
-}
 
 // Overrides default implementation in SpFsObject for efficiency
 bool SpDir::isFile() const
@@ -24,4 +28,22 @@ bool SpDir::isFile() const
 bool SpDir::isDir() const
 {
 	return (true);
+}
+
+list<SpFsObject *> SpDir::ls() const
+{
+	// First open a directory stream
+	DIR *d = opendir(path().fullName().c_str());
+	cout << "Have opened directory stream" << endl;
+	struct dirent *entry;
+	list<SpFsObject *> l;
+	while ((entry = readdir(d)) != NULL) {
+		SpFsObject *o = new SpFsObject;
+		string pathString = entry->d_name;
+		if ((pathString != ".") && (pathString != "..")) {
+			o->setPath(pathString);
+			l.push_back(o);
+		}
+	}
+	return (l);
 }
--- SpDir.h
+++ SpDir.h
@@ -3,14 +3,15 @@
 #ifndef _spdir_h_
 #define _spdir_h_
 
+#include <list>
 #include "SpFsObject.h"
 
 class SpDir : public SpFsObject
 {
 	public:
-		SpDir();
+		SpDir(const string &path = "");
 		~SpDir();
-		SpDir(const string &path);
+		list<SpFsObject *> ls() const;
 		bool isFile() const;
 		bool isDir() const;
 	private:
