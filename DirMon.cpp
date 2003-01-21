28968a55dcf4e7a4a7a01ea93990d5470e823f16
mlandauer <mlandauer>
2003-01-21 23:54:33 +0000

    r240: Added these back from the previously deleted files starting with the Sp prefix

---
+++ DirMon.cpp
@@ -0,0 +1,65 @@
+//  Copyright (C) 2001, 2002 Matthew Landauer. All Rights Reserved.
+//  
+//  This program is free software; you can redistribute it and/or modify it
+//  under the terms of version 2 of the GNU General Public License as
+//  published by the Free Software Foundation.
+//
+//  This program is distributed in the hope that it would be useful, but
+//  WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  Further, any
+//  license provided herein, whether implied or otherwise, is limited to
+//  this program in accordance with the express provisions of the GNU
+//  General Public License.  Patent licenses, if any, provided herein do not
+//  apply to combinations of this program with other product or programs, or
+//  any other product whatsoever.  This program is distributed without any
+//  warranty that the program is delivered free of the rightful claim of any
+//  third person by way of infringement or the like.  See the GNU General
+//  Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write the Free Software Foundation, Inc., 59
+//  Temple Place - Suite 330, Boston MA 02111-1307, USA.
+//
+// $Id$
+
+#include "SpDirMon.h"
+#include "SpDirMonFam.h"
+
+SpDirMon * SpDirMon::construct(const SpDir &d)
+{
+	SpDirMon *m = new SpDirMonFam;
+	if (!m->start(d)) {
+		delete m;
+		return NULL;
+	}
+	return m;
+}
+
+bool SpDirMon::pendingEvent()
+{
+	return (!eventQueue.empty());
+}
+
+SpDirMonEvent SpDirMon::getNextEvent()
+{
+	if (pendingEvent()) {
+		SpDirMonEvent e = eventQueue.front();
+		eventQueue.pop();
+		return e;
+	}
+	else
+		return SpDirMonEvent(SpDirMonEvent::null);
+}
+
+void SpDirMon::notifyChanged(SpFsObjectHandle o)
+{
+	eventQueue.push(SpDirMonEvent(SpDirMonEvent::changed, o));
+}	
+void SpDirMon::notifyDeleted(SpFsObjectHandle o)
+{
+	eventQueue.push(SpDirMonEvent(SpDirMonEvent::deleted, o));
+}	
+void SpDirMon::notifyAdded(SpFsObjectHandle o)
+{
+	eventQueue.push(SpDirMonEvent(SpDirMonEvent::added, o));
+}
---
+++ DirMon.h
@@ -0,0 +1,65 @@
+//  Copyright (C) 2001, 2002 Matthew Landauer. All Rights Reserved.
+//  
+//  This program is free software; you can redistribute it and/or modify it
+//  under the terms of version 2 of the GNU General Public License as
+//  published by the Free Software Foundation.
+//
+//  This program is distributed in the hope that it would be useful, but
+//  WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  Further, any
+//  license provided herein, whether implied or otherwise, is limited to
+//  this program in accordance with the express provisions of the GNU
+//  General Public License.  Patent licenses, if any, provided herein do not
+//  apply to combinations of this program with other product or programs, or
+//  any other product whatsoever.  This program is distributed without any
+//  warranty that the program is delivered free of the rightful claim of any
+//  third person by way of infringement or the like.  See the GNU General
+//  Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write the Free Software Foundation, Inc., 59
+//  Temple Place - Suite 330, Boston MA 02111-1307, USA.
+//
+// $Id$
+
+#ifndef _spdirmon_h_
+#define _spdirmon_h_
+
+#include <queue>
+#include "SpDir.h"
+
+class SpDirMonEvent
+{
+	public:
+		enum SpCode {null, changed, deleted, added};
+		SpDirMonEvent(SpCode c = null, SpFsObjectHandle h = SpFsObjectHandle(NULL)) : code(c), o(h) { }
+		~SpDirMonEvent() { }
+		SpCode getCode() { return code; }
+		SpFsObjectHandle getFsObjectHandle() { return o; }
+	private:
+		SpCode code;
+		SpFsObjectHandle o;
+};
+
+// This class monitors just one directory and its contents
+class SpDirMon
+{
+	public:
+		virtual ~SpDirMon() { }
+		static SpDirMon * construct(const SpDir &d);
+		virtual void update() = 0;
+		bool pendingEvent();
+		SpDirMonEvent getNextEvent();
+	protected:
+		SpDirMon() { }
+		virtual bool start(const SpDir &d) = 0;
+		virtual bool stop() = 0;
+		void notifyChanged(SpFsObjectHandle o);
+		void notifyDeleted(SpFsObjectHandle o);
+		void notifyAdded(SpFsObjectHandle o);
+	private:
+		std::queue<SpDirMonEvent> eventQueue;
+};
+
+
+#endif
