589ab83b9637bd2a034d027accf4cb3021752217
mlandauer <mlandauer>
2003-02-03 05:14:40 +0000

    r278: Adding an image from a new sequence works correctly

--- test/testImageSeqMon.cpp
+++ test/testImageSeqMon.cpp
@@ -91,5 +91,12 @@ void testImageSeqMon::test()
 	sequences = m.getImageSequences();
 	CPPUNIT_ASSERT(sequences.size() == 1);
 	checkSequence(sequences[0], "Cineon", 1024, 768, "/foo/bar/image.#.cin", "1-2");
+	
+	m.imageAdded(CachedImage(ImageDim(512, 512), true, cineonFormat, 100, time,
+		user, userGroup, Path("/foo/bar/image.0003.cin")));
+	sequences = m.getImageSequences();
+	CPPUNIT_ASSERT(sequences.size() == 2);
+	checkSequence(sequences[0], "Cineon", 1024, 768, "/foo/bar/image.#.cin", "1-2");
+	checkSequence(sequences[1], "Cineon", 512, 512, "/foo/bar/image.#.cin", "3");
 }
 
