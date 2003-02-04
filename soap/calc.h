b4f158069770787865c59843647ec564e4bf7bb4
mlandauer <mlandauer>
2003-02-04 01:46:09 +0000

    r287: Client application now uses proxy class generated by gSOAP

--- soap/calc.h
+++ soap/calc.h
@@ -1,7 +1,5 @@
 //gsoap ns service name: calc
-//gsoap ns service namespace: http://websrv.cs.fsu.edu/~engelen/calc.wsdl
-//gsoap ns service location: http://websrv.cs.fsu.edu/~engelen
-//gsoap ns service executable: calc.cgi
+//gsoap ns service location: http://localhost:8080
 //gsoap ns schema namespace: urn:calc
 int ns__add(double a, double b, double *result);
 int ns__sub(double a, double b, double *result);
--- soap/calcclient.cpp
+++ soap/calcclient.cpp
@@ -1,40 +1,40 @@
-#include "soapH.h"
+#include "soapcalcProxy.h"
 #include "calc.nsmap"
 
 const char server[] = "http://localhost:8080";
 
 int main(int argc, char **argv)
-{ struct soap soap;
+{
   double a, b, result;
   if (argc < 4)
   { fprintf(stderr, "Usage: [add|sub|mul|div|pow] num num\n");
     exit(0);
   }
-  soap_init(&soap);
+  calc c;
   a = strtod(argv[2], NULL);
   b = strtod(argv[3], NULL);
   switch (*argv[1])
   { case 'a':
-      soap_call_ns__add(&soap, server, "", a, b, &result);
+      c.add(a, b, &result);
       break;
     case 's':
-      soap_call_ns__sub(&soap, server, "", a, b, &result);
+      c.sub(a, b, &result);
       break;
     case 'm':
-      soap_call_ns__mul(&soap, server, "", a, b, &result);
+      c.mul(a, b, &result);
       break;
     case 'd':
-      soap_call_ns__div(&soap, server, "", a, b, &result);
+      c.div(a, b, &result);
       break;
     case 'p':
-      soap_call_ns__pow(&soap, server, "", a, b, &result);
+      c.pow(a, b, &result);
       break;
     default:
       fprintf(stderr, "Unknown command\n");
       exit(0);
   }
-  if (soap.error)
-    soap_print_fault(&soap, stderr);
+  if (c.soap->error)
+    soap_print_fault(c.soap, stderr);
   else
     printf("result = %g\n", result);
   return 0;
