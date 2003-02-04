da8622d0809b243fb567106c9187765674ae1c52
mlandauer <mlandauer>
2003-02-04 03:53:33 +0000

    r290: Hardcoded a specific call in the client

--- soap/calcclient.cpp
+++ soap/calcclient.cpp
@@ -4,37 +4,14 @@
 
 int main(int argc, char **argv)
 {
-	if (argc < 4) {
-		std::cerr << "Usage: [add|sub|mul|div|pow] num num" << std::endl;
-		exit(0);
-	}
 	calc c;
-	double a = strtod(argv[2], NULL);
-	double b = strtod(argv[3], NULL);
 	double result;
-	switch (*argv[1]) {
-		case 'a':
-			c.add(a, b, &result);
-			break;
-		case 's':
-			c.sub(a, b, &result);
-			break;
-		case 'm':
-			c.mul(a, b, &result);
-			break;
-		case 'd':
-			c.div(a, b, &result);
-			break;
-		case 'p':
-			c.pow(a, b, &result);
-			break;
-		default:
-			std::cerr << "Unknown command" << std::endl;
-			exit(0);
-	}
+	c.add(2, 15, &result);
+	
 	if (c.soap->error)
 		soap_print_fault(c.soap, stderr);
 	else
-		std::cout << "result = " << result << std::endl;
+		std::cout << "2 + 15 = " << result << std::endl;
+
 	return 0;
 }
