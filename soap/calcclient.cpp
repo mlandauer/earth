980ee19a98e817abc545429c1f46ac327e6f1c2c
mlandauer <mlandauer>
2003-02-04 03:42:55 +0000

    r289: Reformatted simple code to be more C++ like

--- soap/calcclient.cpp
+++ soap/calcclient.cpp
@@ -1,41 +1,40 @@
+#include <iostream>
 #include "soapcalcProxy.h"
 #include "calc.nsmap"
 
-const char server[] = "http://localhost:8080";
-
 int main(int argc, char **argv)
 {
-  double a, b, result;
-  if (argc < 4)
-  { fprintf(stderr, "Usage: [add|sub|mul|div|pow] num num\n");
-    exit(0);
-  }
-  calc c;
-  a = strtod(argv[2], NULL);
-  b = strtod(argv[3], NULL);
-  switch (*argv[1])
-  { case 'a':
-      c.add(a, b, &result);
-      break;
-    case 's':
-      c.sub(a, b, &result);
-      break;
-    case 'm':
-      c.mul(a, b, &result);
-      break;
-    case 'd':
-      c.div(a, b, &result);
-      break;
-    case 'p':
-      c.pow(a, b, &result);
-      break;
-    default:
-      fprintf(stderr, "Unknown command\n");
-      exit(0);
-  }
-  if (c.soap->error)
-    soap_print_fault(c.soap, stderr);
-  else
-    printf("result = %g\n", result);
-  return 0;
+	if (argc < 4) {
+		std::cerr << "Usage: [add|sub|mul|div|pow] num num" << std::endl;
+		exit(0);
+	}
+	calc c;
+	double a = strtod(argv[2], NULL);
+	double b = strtod(argv[3], NULL);
+	double result;
+	switch (*argv[1]) {
+		case 'a':
+			c.add(a, b, &result);
+			break;
+		case 's':
+			c.sub(a, b, &result);
+			break;
+		case 'm':
+			c.mul(a, b, &result);
+			break;
+		case 'd':
+			c.div(a, b, &result);
+			break;
+		case 'p':
+			c.pow(a, b, &result);
+			break;
+		default:
+			std::cerr << "Unknown command" << std::endl;
+			exit(0);
+	}
+	if (c.soap->error)
+		soap_print_fault(c.soap, stderr);
+	else
+		std::cout << "result = " << result << std::endl;
+	return 0;
 }
--- soap/calcserver.cpp
+++ soap/calcserver.cpp
@@ -1,61 +1,68 @@
+#include <iostream>
 #include "soapH.h"
 #include "calc.nsmap"
 
 int main(int argc, char **argv)
-{ int m, s; /* master and slave sockets */
-  struct soap soap;
-  soap_init(&soap);
-  m = soap_bind(&soap, NULL, 8080, 100);
-    if (m < 0)
-    { soap_print_fault(&soap, stderr);
-      exit(-1);
+{
+	struct soap soap;
+	soap_init(&soap);
+	int m = soap_bind(&soap, NULL, 8080, 100);
+	if (m < 0) {
+		soap_print_fault(&soap, stderr);
+		exit(-1);
+	}
+	std::cerr << "Socket connection successful: master socket = " << m << std::endl;
+	for ( ; ; ) {
+		int s = soap_accept(&soap);
+		std::cerr << "Socket connection successful: slave socket = " << s << std::endl;
+		if (s < 0)  {
+			soap_print_fault(&soap, stderr);
+			exit(1);
+		} 
+		soap_serve(&soap);
+		soap_end(&soap);
     }
-    fprintf(stderr, "Socket connection successful: master socket = %d\n", m);
-    for ( ; ; )
-    { s = soap_accept(&soap);
-      fprintf(stderr, "Socket connection successful: slave socket = %d\n", s);
-      if (s < 0)
-      { soap_print_fault(&soap, stderr);
-        exit(1);
-      } 
-      soap_serve(&soap);
-      soap_end(&soap);
-    }
-  return 0;
+	return 0;
 } 
 
 int ns__add(struct soap *soap, double a, double b, double *result)
-{ *result = a + b;
-  return SOAP_OK;
+{
+	*result = a + b;
+	return SOAP_OK;
 } 
 
 int ns__sub(struct soap *soap, double a, double b, double *result)
-{ *result = a - b;
-  return SOAP_OK;
+{
+	*result = a - b;
+	return SOAP_OK;
 } 
 
 int ns__mul(struct soap *soap, double a, double b, double *result)
-{ *result = a * b;
-  return SOAP_OK;
+{
+	*result = a * b;
+	return SOAP_OK;
 } 
 
 int ns__div(struct soap *soap, double a, double b, double *result)
-{ if (b)
-    *result = a / b;
-  else
-  { char *s = (char*)soap_malloc(soap, 1024);
-    sprintf(s, "Can't divide %f by %f", a, b);
-    return soap_receiver_fault(soap, "Division by zero", s);
-  }
-  return SOAP_OK;
+{
+	if (b)
+		*result = a / b;
+	else {
+		char *s = (char*)soap_malloc(soap, 1024);
+		sprintf(s, "Can't divide %f by %f", a, b);
+		return soap_receiver_fault(soap, "Division by zero", s);
+	}
+	return SOAP_OK;
 } 
 
 int ns__pow(struct soap *soap, double a, double b, double *result)
-{ *result = pow(a, b);
-  if (soap_errno == EDOM)	/* soap_errno is like errno, but compatible with Win32 */
-  { char *s = (char*)soap_malloc(soap, 1024);
-    sprintf(s, "Can't take the power of %f to %f", a, b);
-    return soap_receiver_fault(soap, "Power function domain error", s);
-  }
-  return SOAP_OK;
+{
+	*result = pow(a, b);
+	/* soap_errno is like errno, but compatible with Win32 */
+	if (soap_errno == EDOM) {
+		char *s = (char*)soap_malloc(soap, 1024);
+		sprintf(s, "Can't take the power of %f to %f", a, b);
+		return soap_receiver_fault(soap, "Power function domain error", s);
+	}
+	return SOAP_OK;
 } 
