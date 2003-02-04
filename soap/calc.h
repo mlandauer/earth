bacb7cd865cd9f31853a28b3ce4d9862d43f0c63
mlandauer <mlandauer>
2003-02-04 03:57:05 +0000

    r291: Removed unused methods in web service

--- soap/calc.h
+++ soap/calc.h
@@ -2,7 +2,3 @@
 //gsoap ns service location: http://localhost:8080
 //gsoap ns schema namespace: urn:calc
 int ns__add(double a, double b, double *result);
-int ns__sub(double a, double b, double *result);
-int ns__mul(double a, double b, double *result);
-int ns__div(double a, double b, double *result);
-int ns__pow(double a, double b, double *result);
--- soap/calcserver.cpp
+++ soap/calcserver.cpp
@@ -30,39 +30,3 @@ int ns__add(struct soap *soap, double a, double b, double *result)
 	*result = a + b;
 	return SOAP_OK;
 } 
-
-int ns__sub(struct soap *soap, double a, double b, double *result)
-{
-	*result = a - b;
-	return SOAP_OK;
-} 
-
-int ns__mul(struct soap *soap, double a, double b, double *result)
-{
-	*result = a * b;
-	return SOAP_OK;
-} 
-
-int ns__div(struct soap *soap, double a, double b, double *result)
-{
-	if (b)
-		*result = a / b;
-	else {
-		char *s = (char*)soap_malloc(soap, 1024);
-		sprintf(s, "Can't divide %f by %f", a, b);
-		return soap_receiver_fault(soap, "Division by zero", s);
-	}
-	return SOAP_OK;
-} 
-
-int ns__pow(struct soap *soap, double a, double b, double *result)
-{
-	*result = pow(a, b);
-	/* soap_errno is like errno, but compatible with Win32 */
-	if (soap_errno == EDOM) {
-		char *s = (char*)soap_malloc(soap, 1024);
-		sprintf(s, "Can't take the power of %f to %f", a, b);
-		return soap_receiver_fault(soap, "Power function domain error", s);
-	}
-	return SOAP_OK;
-} 
