097d10be3a2a7426ba7285a237f031b56be85909
mlandauer <mlandauer>
2003-02-24 05:35:26 +0000

    r300: Defaults test client to server on local machine

--- soap/simpleclient.cpp
+++ soap/simpleclient.cpp
@@ -35,7 +35,7 @@ main(int argc, const char *argv[])
 		if (argc > 1)
 			endpoint = argv[1];
 		else
-			endpoint = "http://easysoap.sourceforge.net/cgi-bin/simpleserver";
+			endpoint = "http://localhost/cgi-bin/simpleserver";
 
 		SOAPProxy proxy(endpoint);
 		SOAPMethod addmethod("add", ns);
