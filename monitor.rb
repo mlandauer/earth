62c3cc25adcdbfa4114967600b93623b0946a539
mlandauer <mlandauer>
2006-10-30 05:53:58 +0000

    r319:  r10069@duck:  matthewl | 2006-10-30 12:31:43 +1100  Do recursive directory parsing now

--- monitor.rb
+++ monitor.rb
@@ -2,13 +2,28 @@ class Monitor
   def initialize(directory)
     @directory = directory
     @filenames = []
+    @monitors = []
   end
   
-  def exist?(relative_path)
-    @filenames.include?(relative_path)
+  def exist?(path)
+    if @filenames.include?(path)
+      true
+    else
+      @monitors.each do |monitor|
+        if monitor.exist?(path)
+          return true
+        end
+      end
+      false
+    end
   end
   
   def update
-    @filenames = Dir.entries(@directory)
+    @filenames, dirs = Dir.entries(@directory).partition{|f| File.file?(File.join(@directory, f))}
+    @filenames.map!{|f| File.join(@directory, f)}
+    dirs.delete(".")
+    dirs.delete("..")
+    @monitors = dirs.map{|d| Monitor.new(File.join(@directory, d))}
+    @monitors.each{|m| m.update}
   end
 end
--- test/test_monitor.rb
+++ test/test_monitor.rb
@@ -5,9 +5,10 @@ class TestMonitor < Test::Unit::TestCase
   def setup
     # Put some test files in the directory test_data
     FileUtils.rm_rf 'test_data'
-    FileUtils.mkdir 'test_data'
+    FileUtils.mkdir_p 'test_data/dir1'
     FileUtils.touch 'test_data/file1'
-    
+    FileUtils.touch 'test_data/dir1/file1'
+
     @monitor = Monitor.new("test_data")
   end
   
@@ -17,14 +18,20 @@ class TestMonitor < Test::Unit::TestCase
   end
 
   def test_empty    
-    assert(!@monitor.exist?('file1'))
-    assert(!@monitor.exist?('file2'))
+    assert(!@monitor.exist?('test_data/file1'))
+    assert(!@monitor.exist?('test_data/file2'))
   end
   
   def test_update
      # It should only update its knowledge of the files on calling update
     @monitor.update
-    assert(@monitor.exist?('file1'))
-    assert(!@monitor.exist?('file2'))    
+    assert(@monitor.exist?('test_data/file1'))
+    assert(!@monitor.exist?('test_data/file2'))    
+  end
+  
+  def test_recursive
+    @monitor.update
+    assert(@monitor.exist?('test_data/dir1/file1'))
+    assert(!@monitor.exist?('test_data/dir1/file2'))
   end
 end
