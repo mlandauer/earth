9e2189d109208ed0185996967cdbab12a9970159
mlandauer <mlandauer>
2006-10-30 05:53:36 +0000

    r318:  r10068@duck:  matthewl | 2006-10-30 11:42:02 +1100  Now takes a snapshot of the directory

--- monitor.rb
+++ monitor.rb
@@ -1,8 +1,14 @@
 class Monitor
   def initialize(directory)
+    @directory = directory
+    @filenames = []
   end
   
-  def exist?(file)
-    File.exist?(file)
+  def exist?(relative_path)
+    @filenames.include?(relative_path)
+  end
+  
+  def update
+    @filenames = Dir.entries(@directory)
   end
 end
--- test/test_monitor.rb
+++ test/test_monitor.rb
@@ -2,17 +2,29 @@ require "monitor.rb"
 require "fileutils.rb"
 
 class TestMonitor < Test::Unit::TestCase
-  def test_simple
+  def setup
     # Put some test files in the directory test_data
     FileUtils.rm_rf 'test_data'
     FileUtils.mkdir 'test_data'
     FileUtils.touch 'test_data/file1'
     
-    monitor = Monitor.new("test_data")
-    assert(monitor.exist?('test_data/file1'))
-    assert(!monitor.exist?('test_data/file2'))
-
+    @monitor = Monitor.new("test_data")
+  end
+  
+  def teardown
     # Tidy up
     FileUtils.rm_rf 'test_data'
+  end
+
+  def test_empty    
+    assert(!@monitor.exist?('file1'))
+    assert(!@monitor.exist?('file2'))
+  end
+  
+  def test_update
+     # It should only update its knowledge of the files on calling update
+    @monitor.update
+    assert(@monitor.exist?('file1'))
+    assert(!@monitor.exist?('file2'))    
   end
 end
