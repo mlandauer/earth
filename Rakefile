315899525071682b0c4a0b9842e937ecf30d12cd
mlandauer <mlandauer>
2006-10-31 06:38:00 +0000

    r339:  r10296@duck:  matthewl | 2006-10-31 16:04:52 +1100  Extracted db configuration

--- Rakefile
+++ Rakefile
@@ -1,14 +1,4 @@
-require "rubygems" 
-require_gem "activerecord" 
-
-ActiveRecord::Base.establish_connection(
-  :adapter  => "mysql",
-  :host     => "localhost",
-  :database => "earth_test",
-  :username => "earth",
-  :password => "earth")
-
-ActiveRecord::Base.logger = Logger.new(STDOUT)
+require 'db_config'
 
 namespace :db do
   desc "Migrate the database through scripts in db/migrate. Target specific version with VERSION=x"
--- test/test_database.rb
+++ test/test_database.rb
@@ -1,19 +1,7 @@
-require "rubygems" 
-require_gem "activerecord" 
-
-class Files < ActiveRecord::Base
-end
+require 'files'
 
 class TestDatabase < Test::Unit::TestCase
   def test_connection
-    ActiveRecord::Base.establish_connection(
-      :adapter  => "mysql",
-      :host     => "localhost",
-      :database => "earth_test",
-      :username => "earth",
-      :password => "earth"
-    )
-    
     file = Files.new
     file.path = 'an/arbitrary/path/to_a_file'
     file.save
