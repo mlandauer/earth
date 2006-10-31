236858f9436e63955c505b217e5849b8aa02c3c9
mlandauer <mlandauer>
2006-10-31 04:50:47 +0000

    r334:  r10113@duck:  matthewl | 2006-10-31 11:28:15 +1100  Added copyright

--- monitor.rb
+++ monitor.rb
@@ -1,3 +1,12 @@
+# Copyright (C) 2006 Rising Sun Pictures and Matthew Landauer.
+# All Rights Reserved.
+#  
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# $Id$
+
 require 'thread'
 require 'snapshot'
 
