a5c063b1640c5748b5bfa6129f9e35c8338c6331
mlandauer <mlandauer>
2006-11-01 03:49:07 +0000

    r346:  r10763@duck:  matthewl | 2006-11-01 12:12:44 +1100  Reduced dependencies in test for MonitorWithDatabase

--- test/test_monitor_with_database.rb
+++ test/test_monitor_with_database.rb
@@ -4,27 +4,17 @@ require "file_info"
 
 class TestMonitorWithDatabase < Test::Unit::TestCase
   def setup
-    # Put some test files in the directory test_data
-    FileUtils.rm_rf 'test_data'
-    FileUtils.mkdir_p 'test_data/dir1'
-    FileUtils.touch 'test_data/file1'
-    FileUtils.touch 'test_data/dir1/file1'
-
     @monitor = MonitorWithDatabase.new("test_data")
   end
   
-  def teardown
-    # Tidy up
-    FileUtils.rm_rf 'test_data'
-  end
-
   # Database should be empty on startup
   def test_empty
     assert(FileInfo.find_all.empty?)
   end
   
   def test_simple
-    @monitor.update
+    @monitor.file_added(File.expand_path('test_data/file1'))
+    @monitor.file_added(File.expand_path('test_data/dir1/file1'))
     files = FileInfo.find_all
     assert_equal(2, files.size)
     assert_equal(File.expand_path('test_data/file1'), files[0].path)
@@ -32,9 +22,9 @@ class TestMonitorWithDatabase < Test::Unit::TestCase
   end
   
   def test_delete
-    @monitor.update
-    FileUtils.rm 'test_data/file1'
-    @monitor.update
+    @monitor.file_added(File.expand_path('test_data/file1'))
+    @monitor.file_added(File.expand_path('test_data/dir1/file1'))
+    @monitor.file_removed(File.expand_path('test_data/file1'))
     files = FileInfo.find_all
     assert_equal(1, files.size)
     assert_equal(File.expand_path('test_data/dir1/file1'), files[0].path)    
