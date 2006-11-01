147699702743a4591566456bd4c3a83249373f11
mlandauer <mlandauer>
2006-11-01 05:51:20 +0000

    r353:  r11374@duck:  matthewl | 2006-11-01 16:16:25 +1100  Stores modification time in database

--- monitor_with_database.rb
+++ monitor_with_database.rb
@@ -8,8 +8,8 @@ class MonitorWithDatabase < FileMonitor
     FileInfo.delete_all
   end
   
-  def file_added(path, name)
-    FileInfo.create(:path => path, :name => name)
+  def file_added(path, name, modified)
+    FileInfo.create(:path => path, :name => name, :modified => modified)
   end
   
   def file_removed(path, name)
--- test/test_monitor_with_database.rb
+++ test/test_monitor_with_database.rb
@@ -6,6 +6,9 @@ class TestMonitorWithDatabase < Test::Unit::TestCase
   def setup
     @dir = File.expand_path('test_data')
     @monitor = MonitorWithDatabase.new(@dir)
+    # 1st of January 2000
+    @time1 = Time.local(2000, 1, 1)
+    @time2 = Time.local(2001, 1, 1)
   end
   
   # Database should be empty on startup
@@ -14,19 +17,21 @@ class TestMonitorWithDatabase < Test::Unit::TestCase
   end
   
   def test_simple
-    @monitor.file_added(@dir, 'file1')
-    @monitor.file_added(File.join(@dir, 'dir1'), 'file1')
+    @monitor.file_added(@dir, 'file1', @time1)
+    @monitor.file_added(File.join(@dir, 'dir1'), 'file1', @time2)
     files = FileInfo.find_all
     assert_equal(2, files.size)
     assert_equal(@dir, files[0].path)
     assert_equal('file1', files[0].name)
+    assert_equal(@time1, files[0].modified)
     assert_equal(File.join(@dir, 'dir1'), files[1].path)
     assert_equal('file1', files[1].name)
+    assert_equal(@time2, files[1].modified)
   end
   
   def test_delete
-    @monitor.file_added(@dir, 'file1')
-    @monitor.file_added(File.join(@dir, 'dir1'), 'file1')
+    @monitor.file_added(@dir, 'file1', @time1)
+    @monitor.file_added(File.join(@dir, 'dir1'), 'file1', @time2)
     @monitor.file_removed(@dir, 'file1')
     files = FileInfo.find_all
     assert_equal(1, files.size)
