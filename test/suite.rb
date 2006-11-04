9965e03957140ef3c05256f9fce768673ef085a8
mlandauer <mlandauer>
2006-11-04 05:53:56 +0000

    r367:  r12616@duck:  matthewl | 2006-11-04 15:18:20 +1100  Adding test suite for running all the tests at once in Eclipse with RDT

---
+++ test/suite.rb
@@ -0,0 +1,8 @@
+# A test suite for running all the unit tests at once
+
+require 'test/unit'
+
+require 'test_database'
+require 'test_monitor_with_database'
+require 'test_monitor'
+require 'test_snapshot'
