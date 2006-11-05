34d3783ea252256c58750805f635a87b0b79e548
mlandauer <mlandauer>
2006-11-05 23:13:40 +0000

    r379:  r12634@duck:  matthewl | 2006-11-05 15:00:32 +1100  Added commented out debugging code

--- file_monitor.rb
+++ file_monitor.rb
@@ -15,16 +15,19 @@ class FileMonitorBase
 
   def file_added(full_path, stat)
     raise("No observer set") if @observer.nil?
+    #puts "File ADDED: #{full_path}"
     @observer.file_added(File.dirname(full_path), File.basename(full_path), stat)
   end
   
   def file_removed(full_path)
     raise("No observer set") if @observer.nil?
+    #puts "File REMOVED: #{full_path}"
     @observer.file_removed(File.dirname(full_path), File.basename(full_path))
   end
   
   def file_changed(full_path, stat)
     raise("No observer set") if @observer.nil?
+    #puts "File CHANGED: #{full_path}"
     @observer.file_changed(File.dirname(full_path), File.basename(full_path), stat)
   end
   
