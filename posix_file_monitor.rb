bbbcfea99d9de44bf33de1ee42b32924ea1e641b
mlandauer <mlandauer>
2006-11-05 23:13:26 +0000

    r378:  r12633@duck:  matthewl | 2006-11-05 13:31:15 +1100  Now posix file monitor can detect added files and directories

--- posix_file_monitor.rb
+++ posix_file_monitor.rb
@@ -1,9 +1,15 @@
 class SnapshotNonRecursive
-  attr_reader :directory_stat, :directory
+  attr_reader :directory_stat, :directory, :subdirectory_names
 
-  def initialize(directory)
+  def deep_copy
+    SnapshotNonRecursive.new(@directory, @stats.clone, @subdirectory_names.clone)
+  end
+  
+  def initialize(directory, stats = Hash.new, subdirectory_names = [])
     @directory = File.expand_path(directory)
     @directory_stat = nil
+    @stats = stats
+    @subdirectory_names = subdirectory_names
   end
   
   def update
@@ -11,15 +17,35 @@ class SnapshotNonRecursive
     if new_stat != @directory_stat
       @directory_stat = new_stat
       # Update contents of directory
+      entries = Dir.entries(@directory)
+      entries.delete(".")
+      entries.delete("..")
+  
+      # Make absolute paths
+      entries.map!{|x| File.join(directory, x)}
+      
+      filenames, @subdirectory_names = entries.partition{|f| File.file?(f)}
+      @stats.clear
+      filenames.each do |f|
+        @stats[f] = File.lstat(f)
+      end
     end
   end
   
   def stat(path)
-    File.lstat(File.expand_path('test_data/file1'))
+    @stats[path]
   end
   
+  def file_names
+    @stats.keys
+  end
+
   def SnapshotNonRecursive.added_files(snap1, snap2)
-    [File.expand_path('test_data/file1')]
+    snap2.file_names - snap1.file_names
+  end
+  
+  def SnapshotNonRecursive.added_directories(snap1, snap2)
+    snap2.subdirectory_names - snap1.subdirectory_names
   end
 end
 
@@ -33,11 +59,12 @@ class PosixFileMonitor < FileMonitorBase
     # Get the stat information of all the directories from the current snapshot
     # and find the directories which have different stat info now
     for snapshot in @snapshots
-      old_snapshot = snapshot.clone
+      old_snapshot = snapshot.deep_copy
       snapshot.update
       SnapshotNonRecursive.added_files(old_snapshot, snapshot).each {|x| file_added(x, snapshot.stat(x))}
+      for added_directory in SnapshotNonRecursive.added_directories(old_snapshot, snapshot)
+        @snapshots << SnapshotNonRecursive.new(added_directory)
+      end
     end
-    
-    file_added(File.expand_path('test_data/dir1/file1'), File.lstat(File.expand_path('test_data/dir1/file1')))
   end
 end
