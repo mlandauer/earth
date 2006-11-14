b03f77ed3982a67c1fe2b1fa93ba50a4f88ab24c
mlandauer <mlandauer>
2006-11-14 06:35:28 +0000

    r456:  r12797@duck:  matthewl | 2006-11-14 17:29:55 +1100  Added method for finding this server to Server

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,2 +1,7 @@
+require 'socket'
+
 class Server < ActiveRecord::Base
+  def Server.find_this_server
+    Server.find_by_name(Socket.gethostname)
+  end
 end
--- test/unit/server_test.rb
+++ test/unit/server_test.rb
@@ -3,8 +3,11 @@ require File.dirname(__FILE__) + '/../test_helper'
 class ServerTest < Test::Unit::TestCase
   fixtures :servers
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_find_this_server
+    Server.create(:name => Socket.gethostname, :watch_directory => "/foo")
+    Server.create(:name => "foo", :watch_directory => "/bar")
+    server = Server.find_this_server
+    assert_equal(Socket.gethostname, server.name)
+    assert_equal("/foo", server.watch_directory)
   end
 end
