18c308322df3a54a83658a87d9027050212ae825
mlandauer <mlandauer>
2006-11-14 06:33:09 +0000

    r449:  r12790@duck:  matthewl | 2006-11-14 15:26:49 +1100  Assuming that iterator is only invalidated by removing from hash during loop

--- daemon/posix_file_monitor.rb
+++ daemon/posix_file_monitor.rb
@@ -1,8 +1,8 @@
 class PosixFileMonitor < FileMonitor
   def initialize(directory, observer)
     super(observer)
-    @snapshots = {File.expand_path(directory) => Snapshot.new}
     dir = directory_added(File.expand_path(directory))
+    @snapshots = {File.expand_path(directory) => Snapshot.new}
     @directories = {File.expand_path(directory) => dir}
   end
   
@@ -10,8 +10,8 @@ class PosixFileMonitor < FileMonitor
     @snapshots[directory].subdirectory_names.each {|x| remove_directory(File.join(directory, x))}
     # TODO: Check the line below
     @snapshots[directory].file_names.each {|x| file_removed(File.join(directory, x))}
-    @snapshots.delete(directory)
     directory_removed(@directories[directory])
+    @snapshots.delete(directory)
     @directories.delete(directory)
   end
   
@@ -39,20 +39,18 @@ class PosixFileMonitor < FileMonitor
       old_snapshot = snapshot.deep_copy
       snapshot.update(path)
 
-      added_directories += Difference.added_directories(old_snapshot, snapshot).map{|d| File.join(path, d)}
-      removed_directories += Difference.removed_directories(old_snapshot, snapshot).map{|d| File.join(path, d)}
-      added_files += Difference.added_files(old_snapshot, snapshot).map {|x| FileInfo.new(directory, x, snapshot.stat(x))}
-      
-      Difference.removed_files(old_snapshot, snapshot).each {|x| file_removed(directory, x)}
       Difference.changed_files(old_snapshot, snapshot).each {|x| file_changed(directory, x, snapshot.stat(x))}
+    
+      Difference.added_directories(old_snapshot, snapshot).map{|d| File.join(path, d)}.each {|x| add_directory(x)}
+      Difference.added_files(old_snapshot, snapshot).map {|x| FileInfo.new(directory, x, snapshot.stat(x))}.each {|x| file_added(x.directory, x.name, x.stat)}
+      Difference.removed_files(old_snapshot, snapshot).each {|x| file_removed(directory, x)}
+      removed_directories += Difference.removed_directories(old_snapshot, snapshot).map{|d| File.join(path, d)}
     end
     
-    # Doing this outside the loop above so that we are not adding and removing from @snapshots
+    # Doing this outside the loop above so that we are not removing from @snapshots
     # while we're in the middle of the loop
-    # TODO: Take account of possible duplicates in added_directories and removed_directories
+    # TODO: Take account of possible duplicates in removed_directories
     # TODO: file_added and directory_added messages do appear currently in a strange order
-    added_directories.each {|x| add_directory(x)}
-    added_files.each {|x| file_added(x.directory, x.name, x.stat)}
     removed_directories.each {|x| remove_directory(x)}
   end
 end
