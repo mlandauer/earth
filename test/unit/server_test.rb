7e2188007e647362712ee694b59ce6f9491dea37
mlandauer <mlandauer>
2006-11-15 00:17:49 +0000

    r458:  r12818@duck:  matthewl | 2006-11-15 09:38:40 +1100  Moved records for testing Server over to the fixtures

--- test/unit/server_test.rb
+++ test/unit/server_test.rb
@@ -4,16 +4,13 @@ class ServerTest < Test::Unit::TestCase
   fixtures :servers
 
   def test_find_this_server
-    Server.create(:name => Server.this_hostname, :watch_directory => "/foo")
-    Server.create(:name => "foo", :watch_directory => "/bar")
     server = Server.this_server
-    assert_equal(Socket.gethostname, server.name)
+    assert_equal(Server.this_hostname, server.name)
     assert_equal("/foo", server.watch_directory)
   end
   
   def test_server_not_in_db
-    Server.create(:name => "foo", :watch_directory => "/bar")
-    # This test assumes that you wouldn't call your machine foo!
+    Server.this_server.destroy
     assert_raise(RuntimeError) { Server.this_server }
   end  
 end
