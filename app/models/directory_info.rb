fda8120f6b6132283df35e7abf10f438f743dbb9
mlandauer <mlandauer>
2006-11-15 00:21:22 +0000

    r467:  r12827@duck:  matthewl | 2006-11-15 11:17:59 +1100  Oops. left things in a broken state last commit

--- app/models/directory_info.rb
+++ app/models/directory_info.rb
@@ -1,3 +1,8 @@
 class DirectoryInfo < ActiveRecord::Base
   belongs_to :server
+
+  # Convenience method for setting all the fields associated with stat in one hit
+  def stat=(stat)
+    self.modified = stat.mtime unless stat.nil?
+  end
 end
