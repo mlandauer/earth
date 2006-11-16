895b5c7d272edefbfb8dbab99ad74983bf0a3d46
mlandauer <mlandauer>
2006-11-16 02:39:35 +0000

    r497:  r12886@duck:  matthewl | 2006-11-16 10:21:10 +1100  Do the adding and removing of snapshots outside main loop

--- daemon/posix_file_monitor.rb
+++ daemon/posix_file_monitor.rb
@@ -4,6 +4,9 @@ class PosixFileMonitor < FileMonitor
     directory = File.expand_path(directory)
 
     @snapshots = {directory => Snapshot.new(self, @observer.directory_added(directory))}
+    # Keeps track of added an removed directories in each update cycle
+    @added_snapshots = Hash.new
+    @removed_paths = []
   end
   
   # Diverting messages from Snapshot objects
@@ -12,7 +15,7 @@ class PosixFileMonitor < FileMonitor
 
     snapshot = Snapshot.new(self, directory)
     snapshot.update
-    @snapshots[path] = snapshot
+    @added_snapshots[path] = snapshot
     directory
   end
 
@@ -22,12 +25,20 @@ class PosixFileMonitor < FileMonitor
 
     snapshot.subdirectories.each_value {|directory| directory_removed(directory)}
     snapshot.file_names.each {|x| file_removed(directory, x)}
-    @snapshots.delete(directory.path)
+    @removed_paths << directory.path
 
     @observer.directory_removed(directory)
   end
   
   def update
-    @snapshots.each_value {|snapshot| snapshot.update} 
+    @snapshots.each_value {|snapshot| snapshot.update}
+    # Do the adding and remove of snapshots outside of the loop above
+    # as otherwise this can cause:
+    # RuntimeError: hash modified during iteration.
+    # TODO: Understand "hash modified during iteration" properly
+    @snapshots.merge!(@added_snapshots)
+    @added_snapshots.clear
+    @removed_paths.each {|p| @snapshots.delete(p)}
+    @removed_paths.clear
   end
 end
