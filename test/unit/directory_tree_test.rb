1724698f7b5556b06d5d1ccff90ae9f5beea7b8d
mlandauer <mlandauer>
2006-11-20 04:43:02 +0000

    r506:  r12910@duck:  matthewl | 2006-11-18 23:54:20 +101800  Added delete method

--- test/unit/directory_tree_test.rb
+++ test/unit/directory_tree_test.rb
@@ -1,5 +1,5 @@
 class DirectoryTree 
-  attr_reader :path, :value
+  attr_reader :path, :value, :children
 
   def initialize(path, value)
     @path = path
@@ -11,6 +11,18 @@ class DirectoryTree
     subtree = find(File.dirname(path))
     raise "Couldn't find to add" if subtree.nil?
     subtree.add_to_root(File.basename(path), value)
+  end
+  
+  def delete(path)
+    subtree_parent = find(File.dirname(path))
+    subtree = find(path)
+    if subtree_parent.nil? or subtree.nil?
+      raise "Can not delete" 
+    end
+    if !subtree.children.empty?
+      raise "Can not delete directory with subdirectories"
+    end
+    subtree_parent.children.delete(subtree)
   end
   
   # Add an item to the root of this tree
@@ -58,6 +70,16 @@ class DirectoryTreeTest < Test::Unit::TestCase
   
   def test_add_non_existent
     assert_raise(RuntimeError) {@t.add("/usr/images/foo/foo2", "foo")}
+  end
+  
+  def test_delete
+    @t.delete("/usr/images/c1/g1.1")
+    assert_raise(RuntimeError) {@t.delete("/usr/images/c2")}
+    assert_raise(RuntimeError) {@t.delete("foo")}
+    @t.delete("/usr/images/c2/g2.1")
+    a = []
+    @t.each {|x| a << x}
+    assert_equal(["Grandchild 1.2", "Child 1", "Child 2", "Parent"], a)
   end
   
   def test_find
