20083e42fb9990d2f13a3eb67fc03ade65136d7e
mlandauer <mlandauer>
2006-11-20 04:41:52 +0000

    r503:  r12907@duck:  matthewl | 2006-11-18 22:10:45 +101800  Added path to each item in the tree

--- test/unit/directory_tree_test.rb
+++ test/unit/directory_tree_test.rb
@@ -1,13 +1,15 @@
 class DirectoryTree 
   attr_reader :value
 
-  def initialize(value) 
+  def initialize(path, value)
+    @path = path
     @value = value 
     @children = [] 
   end 
 
-  def <<(value) 
-    subtree = DirectoryTree.new(value) 
+  # Add an item to the root of this tree
+  def add_to_root(path, value) 
+    subtree = DirectoryTree.new(path, value) 
     @children << subtree 
     return subtree 
   end
@@ -24,12 +26,12 @@ end
 
 class DirectoryTreeTest < Test::Unit::TestCase
   def setup
-    @t = DirectoryTree.new("Parent") 
-    child1 = @t << "Child 1" 
-    child1 << "Grandchild 1.1" 
-    child1 << "Grandchild 1.2" 
-    child2 = @t << "Child 2" 
-    child2 << "Grandchild 2.1" 
+    @t = DirectoryTree.new("/usr/images", "Parent") 
+    child1 = @t.add_to_root("c1", "Child 1")
+    child1.add_to_root("g1.1", "Grandchild 1.1")
+    child1.add_to_root("g1.2", "Grandchild 1.2")
+    child2 = @t.add_to_root("c2", "Child 2")
+    child2.add_to_root("g2.1", "Grandchild 2.1")
   end
   
   def test_each
