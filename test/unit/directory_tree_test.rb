3a559711e152e90e81def1d30b2de3759db69521
mlandauer <mlandauer>
2006-11-20 04:42:14 +0000

    r504:  r12908@duck:  matthewl | 2006-11-18 22:47:26 +101800  Added find method

--- test/unit/directory_tree_test.rb
+++ test/unit/directory_tree_test.rb
@@ -1,5 +1,5 @@
 class DirectoryTree 
-  attr_reader :value
+  attr_reader :path, :value
 
   def initialize(path, value)
     @path = path
@@ -9,7 +9,7 @@ class DirectoryTree
 
   # Add an item to the root of this tree
   def add_to_root(path, value) 
-    subtree = DirectoryTree.new(path, value) 
+    subtree = DirectoryTree.new(@path + "/" + path, value) 
     @children << subtree 
     return subtree 
   end
@@ -21,7 +21,23 @@ class DirectoryTree
       child_node.each { |e| yield e } 
     end 
     yield value 
-  end 
+  end
+  
+  # Naive implementation of find (searches through
+  # every node)
+  def find(path)
+    if @path == path
+      return self
+    else
+      @children.each do |child|
+        s = child.find(path)
+        if s
+          return s
+        end
+      end
+    end
+    return nil
+  end
 end 
 
 class DirectoryTreeTest < Test::Unit::TestCase
@@ -32,6 +48,12 @@ class DirectoryTreeTest < Test::Unit::TestCase
     child1.add_to_root("g1.2", "Grandchild 1.2")
     child2 = @t.add_to_root("c2", "Child 2")
     child2.add_to_root("g2.1", "Grandchild 2.1")
+  end
+  
+  def test_find
+    assert_equal("Parent", @t.find("/usr/images").value)
+    assert_equal("Grandchild 1.1", @t.find("/usr/images/c1/g1.1").value)
+    assert_equal(nil, @t.find("/usr/images/foo"))
   end
   
   def test_each
