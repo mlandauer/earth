62d577f5ac9861a9a75bb764e07046e396dcc950
mlandauer <mlandauer>
2006-11-27 06:45:42 +0000

    r554:  r13010@duck:  matthewl | 2006-11-27 17:45:02 +1100  Now uses the same units for all the sizes in directories

--- app/controllers/directories_controller.rb
+++ app/controllers/directories_controller.rb
@@ -5,6 +5,12 @@ class DirectoriesController < ApplicationController
   
   def size
     @directory = Directory.find(params[:id])
+    @directory_size = @directory.size
     @children_and_sizes = @directory.children.map{|x| [x, x.recursive_size]}
+    @max_size = @children_and_sizes.map{|x| x[1]}.max
+    # Include the size of this directory
+    if @directory_size > @max_size
+      @max_size = @directory_size
+    end
   end
 end
