09ce4efa21b3942a8f1a5c5416803885334a6e90
mlandauer <mlandauer>
2006-11-27 03:30:11 +0000

    r544:  r12984@duck:  matthewl | 2006-11-26 23:17:30 +1100  Only passing one variable to view now

--- app/controllers/directory_info_controller.rb
+++ app/controllers/directory_info_controller.rb
@@ -4,10 +4,8 @@ class DirectoryInfoController < ApplicationController
   end
   
   def size
-    # If no id is given will find all the directories without parents (the root ones)
     if params[:id]
-      @this_directory = DirectoryInfo.find(params[:id])
+      @directory = DirectoryInfo.find(params[:id])
     end
-    @directories = DirectoryInfo.find_all_by_parent_id(params[:id])
   end
 end
