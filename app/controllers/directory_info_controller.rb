61061380eabe41beae51aab1f7ee3a2a946885f3
mlandauer <mlandauer>
2006-11-27 03:29:35 +0000

    r542:  r12982@duck:  matthewl | 2006-11-26 14:29:42 +1100  Some small changes...

--- app/controllers/directory_info_controller.rb
+++ app/controllers/directory_info_controller.rb
@@ -1,12 +1,13 @@
 class DirectoryInfoController < ApplicationController
   def index
     redirect_to :action => :size
-    #size
-    #render :action => :size
   end
   
   def size
     # If no id is given will find all the directories without parents (the root ones)
+    if params[:id]
+      @this_directory = DirectoryInfo.find(params[:id])
+    end
     @directories = DirectoryInfo.find_all_by_parent_id(params[:id])
   end
 end
