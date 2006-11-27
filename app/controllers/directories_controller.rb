d53b37ef4205d14e1340b7bc237ceb8b0f7750e9
mlandauer <mlandauer>
2006-11-27 06:45:15 +0000

    r553:  r13009@duck:  matthewl | 2006-11-27 17:06:17 +1100  Calculates directory sizes in controller

--- app/controllers/directories_controller.rb
+++ app/controllers/directories_controller.rb
@@ -5,5 +5,6 @@ class DirectoriesController < ApplicationController
   
   def size
     @directory = Directory.find(params[:id])
+    @children_and_sizes = @directory.children.map{|x| [x, x.recursive_size]}
   end
 end
