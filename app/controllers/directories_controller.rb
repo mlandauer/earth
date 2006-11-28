f6597e6ccb40abc3286dfa405d35ac695ab22da5
mlandauer <mlandauer>
2006-11-28 23:07:29 +0000

    r555:  r13013@duck:  matthewl | 2006-11-27 18:07:25 +1100  Sorts the directories by size now

--- app/controllers/directories_controller.rb
+++ app/controllers/directories_controller.rb
@@ -7,7 +7,9 @@ class DirectoriesController < ApplicationController
     @directory = Directory.find(params[:id])
     @directory_size = @directory.size
     @children_and_sizes = @directory.children.map{|x| [x, x.recursive_size]}
-    @max_size = @children_and_sizes.map{|x| x[1]}.max
+    # Sort the directories so that the largest comes first
+    @children_and_sizes.sort!{|a,b| b[1] <=> a[1]}
+    @max_size = @children_and_sizes.first[1]
     # Include the size of this directory
     if @directory_size > @max_size
       @max_size = @directory_size
