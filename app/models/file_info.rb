2555ff53ff39b310a50595816d5d0e39b889e55c
mlandauer <mlandauer>
2006-12-11 04:26:15 +0000

    r615: Now displays user name in the result of searches on file

--- app/models/file_info.rb
+++ app/models/file_info.rb
@@ -1,5 +1,6 @@
 class FileInfo < ActiveRecord::Base
   belongs_to :directory
+  composed_of :user, :class_name => "User", :mapping => [%w(uid uid)]
   
   Stat = Struct.new(:mtime, :size, :uid, :gid)
   class Stat
