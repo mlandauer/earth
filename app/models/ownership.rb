183a6f207c708e9cb7f654296df7a479542bd4cd
mlandauer <mlandauer>
2006-12-11 07:17:57 +0000

    r624: Prettier form of the ldap configuration in earth.yml

--- app/models/ownership.rb
+++ app/models/ownership.rb
@@ -12,21 +12,21 @@ class Ownership
   end
   
   def user_name
-    lookup(config["ldap_user_base"], config["ldap_user_id_field"], config["ldap_user_name_field"], uid)
+    lookup config["ldap_user_lookup"], uid
   end
   
   def group_name
-    lookup config["ldap_group_base"], config["ldap_group_id_field"], config["ldap_group_name_field"], gid
+    lookup config["ldap_group_lookup"], gid
   end
   
   private
   
-  def lookup(base, id_field, name_field, id)
+  def lookup(lookup_config, id)
     #TODO: Don't make a new connection to the server for every request
     if config["ldap_server_name"]
       LDAP::Conn.new(config["ldap_server_name"], config["ldap_server_port"]).bind do |conn|
-        conn.search(base, LDAP::LDAP_SCOPE_SUBTREE, "#{id_field}=#{id}") do |e|
-          return e.vals(name_field)[0]
+        conn.search(lookup_config["base"], LDAP::LDAP_SCOPE_SUBTREE, "#{lookup_config["id_field"]}=#{id}") do |e|
+          return e.vals(lookup_config["name_field"])[0]
         end
       end
     else
