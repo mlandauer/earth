9df10a54a1c3b00fdd17033a0095ac0d173475ce
mlandauer <mlandauer>
2006-12-11 04:43:58 +0000

    r616: Ugly hack...

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -7,7 +7,9 @@ class UserTest < Test::Unit::TestCase
   end
   
   def test_no_ldap_configured
+    saved_ldap_server_name = Ownership.ldap_server_name
     User.ldap_server_name = nil
     assert_equal("100", User.new(100).name)
+    User.ldap_server_name = saved_ldap_server_name
   end
 end
