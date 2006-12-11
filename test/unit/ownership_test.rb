9af012db1bf6434cd52133e79e58d6c6b647ed01
mlandauer <mlandauer>
2006-12-11 15:12:33 +0000

    r635: By default now ldap is disabled and tests will pass without it

--- test/unit/ownership_test.rb
+++ test/unit/ownership_test.rb
@@ -4,8 +4,11 @@ class OwnershipTest < Test::Unit::TestCase
   # TODO: Tests currently hardcoded for a specific user. Make them more general.
   def test_name
     ownership = Ownership.new(3054, 20)
-    assert_equal("kenji", ownership.user_name)
-    assert_equal("users", ownership.group_name)
+    # Only do the following tests if ldap is configured
+    if Ownership.config["ldap_server_name"]
+      assert_equal("kenji", ownership.user_name)
+      assert_equal("users", ownership.group_name)
+    end
   end
   
   def test_no_ldap_configured
