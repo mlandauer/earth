f3d49e2fd805429effb9256c69ee91af88efc6fc
mlandauer <mlandauer>
2006-12-11 05:46:59 +0000

    r622: Deleting canopy reference code that we don't need anymore

--- canopy/script/load_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Wrapper script so that we can set the env variables
-# properly as CRON just sets a basic SHELL and PATH
-
-export PATH=$PATH:/usr/local/bin:/usr/local/pgsql/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/pgsql/lib
-
-/usr/local/bin/ruby /var/www/rspintranet/script/load_data.rb
