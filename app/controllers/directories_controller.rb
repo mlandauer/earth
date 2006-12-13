95b762ed02801586bbc1b0e746af24cf272d17a3
kenji_oates <kenji_oates>
2006-12-13 01:09:56 +0000

    r663: Added export to CSV, VERY ugly

--- app/controllers/directories_controller.rb
+++ app/controllers/directories_controller.rb
@@ -27,4 +27,40 @@ class DirectoriesController < ApplicationController
       wants.xml {render :action => "size.rxml", :layout => false}
     end
   end
+    
+  def save_as_csv
+    if params[:server] && params[:path]
+      server = Server.find_by_name(params[:server])
+      raise "Couldn't find server #{params[:server]}" if server.nil?
+      @directory = server.directories.find_by_path(params[:path])
+      raise "Couldn't find directory #{params[:path]}" if @directory.nil?
+    else
+      @directory = Directory.find(params[:id])
+    end
+    
+    @directory_size = @directory.size
+    @children_and_sizes = @directory.children.map{|x| [x, x.recursive_size]}
+    # Sort the directories so that the largest comes first
+    @children_and_sizes.sort!{|a,b| b[1] <=> a[1]}
+    if @children_and_sizes.empty?
+      @max_size = 0
+    else
+      @max_size = @children_and_sizes.first[1]
+    end
+    if @max_size == 0
+      @max_size = 1
+    end
+    
+    @csv_report = StringIO.new
+    CSV::Writer.generate(@csv_report, ',') do |csv|
+      csv << ['Directory', 'Size (bytes)']
+      for directory, size in @children_and_sizes
+        csv << [directory.name, size]
+      end
+    end
+    
+    @csv_report.rewind
+    send_data(@csv_report.read, :type => 'text/csv; charset=iso-8859-1; header=present', :filename => 'earth_report.csv', :disposition => 'downloaded')
+  end
+  
 end
