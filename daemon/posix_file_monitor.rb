cf0552bf7b89a019ed2a246de534919e79dd94ca
mlandauer <mlandauer>
2006-12-15 01:21:21 +0000

    r691: This is good. We don't need DirectoryTree anymore since we are using the new iterator each in Directory mixed with sensible caching of the "children" method to iterate over the directory structure from the leaves to the root.
    This should mean a speedup and a reduced memory consumption for the daemon

--- daemon/posix_file_monitor.rb
+++ daemon/posix_file_monitor.rb
@@ -1,27 +1,21 @@
 class PosixFileMonitor
   def initialize(directory)
-    @server = directory.server
-    snapshot = Snapshot.new(directory, self)
-    @snapshots = DirectoryTree.new(directory.path, snapshot)
-    # Retrieve all subdirectories from the database
-    directory.all_children.each do |subdir|
-      snapshot = Snapshot.new(subdir, self)
-      @snapshots.add(subdir.path, snapshot)
-    end
+    @directory = directory
   end
   
   def directory_added(directory)
     snapshot = Snapshot.new(directory, self)
-    @snapshots.add(directory.parent.path, directory.name, snapshot)
     snapshot.update
     directory
   end
 
   def directory_removed(directory)
-    @snapshots.delete(directory.path)
   end
   
   def update
-    @snapshots.clone.each {|snapshot| snapshot.update}
+    @directory.each do |directory|
+      snapshot = Snapshot.new(directory, self)
+      snapshot.update
+    end
   end
 end
