e49363195fb452ec7084b5d0ad15c4cab3f44277
mlandauer <mlandauer>
2006-12-15 21:39:46 +0000

    r708:  r13221@duck:  matthewl | 2006-12-16 08:20:35 +1100  Removed some unnecessary explicit configuration

--- app/models/earth/server.rb
+++ app/models/earth/server.rb
@@ -2,7 +2,7 @@ require 'socket'
 
 module Earth
   class Server < ActiveRecord::Base
-    has_many :directories, :class_name => "Earth::Directory", :dependent => :destroy
+    has_many :directories, :dependent => :destroy
   
     def Server.this_server
       Server.find_or_create_by_name(this_hostname)
