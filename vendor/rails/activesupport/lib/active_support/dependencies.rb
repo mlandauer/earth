222f4ef87c8aed1da069cce4150e9eada1b3465d
matthewl <matthewl>
2007-02-19 03:00:35 +0000

    r955:  r14452@duck:  matthewl | 2007-02-19 12:32:13 +1100  Updated to Rails 1.2.2. Fixes ticket #125.

--- vendor/rails/activesupport/lib/active_support/dependencies.rb
+++ vendor/rails/activesupport/lib/active_support/dependencies.rb
@@ -47,7 +47,6 @@ module Dependencies #:nodoc:
   mattr_accessor :log_activity
   self.log_activity = false
   
-  # :nodoc:
   # An internal stack used to record which constants are loaded by any block.
   mattr_accessor :constant_watch_stack
   self.constant_watch_stack = []
@@ -382,7 +381,7 @@ module Dependencies #:nodoc:
     end
   end
   
-  class LoadingModule
+  class LoadingModule #:nodoc:
     # Old style environment.rb referenced this method directly.  Please note, it doesn't
     # actualy *do* anything any more.
     def self.root(*args)
