fad95e955db134d289d9b0c06d66156752709942
matthewl <matthewl>
2007-02-23 01:58:24 +0000

    r988: Extracted method. Ready for doing automatic version numbering from a release script

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -65,8 +65,12 @@ module ApplicationHelper
 
   # This depends on the application running from a checked out svn version and
   # the command "svnversion" being available on the path
-  def ApplicationHelper::earth_version
+  def ApplicationHelper::earth_version_svn
     IO::popen("svnversion #{RAILS_ROOT}") { |f| f.readline }
+  end
+  
+  def ApplicationHelper::earth_version
+    earth_version_svn
   end
 
   def ApplicationHelper::get_browser_warning(request)
