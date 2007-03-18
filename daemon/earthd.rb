f533890ab6f6bf93612b6cf628ad61f85560c8e4
julians <julians>
2007-03-18 06:49:53 +0000

    r1079: Added GPL boilerplate header to all .rb, .rhtml and .rxml files; added GPL v2 text in file COPYING; cf http://producingoss.com/html-chunk/license-quickstart.html#license-quickstart-applying

--- daemon/earthd.rb
+++ daemon/earthd.rb
@@ -1,13 +1,20 @@
 #!/usr/bin/env ruby
-#
-# Copyright (C) 2006 Rising Sun Pictures and Matthew Landauer.
-# All Rights Reserved.
-#  
-# This program is free software; you can redistribute it and/or modify it
-# under the terms of version 2 of the GNU General Public License as
-# published by the Free Software Foundation.
-#
-# $Id$
+
+# Copyright (C) 2007 Rising Sun Pictures and Matthew Landauer
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 require 'optparse'
 require 'ostruct'
