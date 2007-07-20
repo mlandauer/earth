459ddfad09f8d24f95eb4e66304407969742f93f
bruno <bruno>
2007-07-20 03:47:24 +0000

    r1124: 2007-07-20  Bruno Mattarollo <bruno.mattarollo@rsp.com.au>
      * test/unit/quote_bad_characters_test.rb: For some reason there wasn't a require test_helper there and this was breaking the tests on the CI server.

--- test/unit/quote_bad_characters_test.rb
+++ test/unit/quote_bad_characters_test.rb
@@ -14,6 +14,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
+require File.dirname(__FILE__) + '/../test_helper'
+
 class QuoteBadCharactersTest < Test::Unit::TestCase
   def setup
     @quote = QuoteBadCharacters.new
