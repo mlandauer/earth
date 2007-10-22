7bdd74bdede8e45a396ae32ec4a3fa39933725aa
lachiec <lachiec>
2007-10-22 02:22:31 +0000

    r1170: moved under piston management, added hack to suppress double loading

---
+++ vendor/plugins/redhillonrails_core/init.rb
@@ -0,0 +1,30 @@
+# why why why?! 
+# Under test, the plugin seems to be included twice, meaning that ActiveRecord::SchemaDumper#tables (for example) is double method-chained
+# This inevitably reults in a recursive infinite loop.
+# We guard against it here:
+
+@@initted_rh ||= false
+unless @@initted_rh
+
+  ActiveRecord::Base.send(:include, RedHillConsulting::Core::ActiveRecord::Base)
+  ActiveRecord::Schema.send(:include, RedHillConsulting::Core::ActiveRecord::Schema)
+  ActiveRecord::SchemaDumper.send(:include, RedHillConsulting::Core::ActiveRecord::SchemaDumper)
+  ActiveRecord::ConnectionAdapters::IndexDefinition.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::IndexDefinition)
+  ActiveRecord::ConnectionAdapters::TableDefinition.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::TableDefinition)
+  ActiveRecord::ConnectionAdapters::Column.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::Column)
+  ActiveRecord::ConnectionAdapters::AbstractAdapter.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::AbstractAdapter)
+  ActiveRecord::ConnectionAdapters::SchemaStatements.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::SchemaStatements)
+
+  if defined?(ActiveRecord::ConnectionAdapters::PostgreSQLAdapter) then
+    ActiveRecord::ConnectionAdapters::PostgreSQLAdapter.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::PostgresqlAdapter)
+  end
+  if defined?(ActiveRecord::ConnectionAdapters::MysqlAdapter) then
+    ActiveRecord::ConnectionAdapters::MysqlColumn.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::MysqlColumn)
+    ActiveRecord::ConnectionAdapters::MysqlAdapter.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::MysqlAdapter)
+  end
+  if defined?(ActiveRecord::ConnectionAdapters::SQLite3Adapter) then
+    ActiveRecord::ConnectionAdapters::SQLite3Adapter.send(:include, RedHillConsulting::Core::ActiveRecord::ConnectionAdapters::Sqlite3Adapter)
+  end
+
+end
+@@initted_rh = true
